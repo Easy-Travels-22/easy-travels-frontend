7c04cbffbf09d78b0cc9b43f63468b6fc4258aef
andrea-twl <tan.wanling.andrea@gmail.com>
2022-05-05 18:38:37 +0800

    add: addtrip function

--- screens/CreateTripScreen.js
+++ screens/CreateTripScreen.js
@@ -14,15 +14,19 @@ import { useState } from "react";
 import { continents, countries } from "countries-list";
 import { FontAwesome } from "@expo/vector-icons";
 
+import uuid from "react-native-uuid";
 import DatePicker from "react-native-neat-date-picker";
 import global from "../assets/styles/global";
 
-export default function CreateTripScreen() {
+export default function CreateTripScreen({ route, navigation }) {
+  const { addTrip } = route.params;
   const { width, height } = Dimensions.get("window");
   const [modalOpen, setModalOpen] = useState(false);
   const [dateRange, setDateRange] = useState();
+  const [name, setName] = useState();
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+  const [description, setDescription] = useState();
 
   const handleDateConfirm = ({ startDate, endDate }) => {
     setStartDate(startDate);
@@ -39,96 +43,117 @@ export default function CreateTripScreen() {
 
     return diffInDays;
   };
+
+  const handleSubmit = () => {
+    const obj = {
+      key: uuid.v4(),
+      name: name,
+      startDate: startDate,
+      endDate: endDate,
+      description: description,
+    };
+    console.log(obj);
+    addTrip(obj);
+  };
   return (
-    <TouchableWithoutFeedback
-      style={{ height: "100%" }}
-      onPress={Keyboard.dismiss()}
-    >
-      <View style={styles.container}>
-        <Text style={[global.h3, { marginBottom: 27 }]}>Plan a Trip</Text>
-        <View style={styles.fieldContainer}>
-          <TextInput
-            style={styles.fieldInput}
-            placeholder={"Trip name"}
-            placeholderTextColor="black"
-          />
-        </View>
-        <TouchableOpacity
-          style={{
-            ...styles.fieldContainer,
-            height: 50,
-            paddingLeft: 12,
-            marginBottom: 0,
-          }}
-          activeOpacity={1}
-          onPress={handleSelectDate}
-        >
-          <Text style={{ width: "37%", fontFamily: "RobotoLight" }}>{`From:\n${
-            startDate ? startDate.toDateString() : ""
-          }`}</Text>
-          <Text style={{ width: "37%", fontFamily: "RobotoLight" }}>{`To:\n${
-            endDate ? endDate.toDateString() : ""
-          }`}</Text>
-          <View
-            style={{
-              backgroundColor: "grey",
-              height: 50,
-              width: 60,
-              borderRadius: 8,
-              borderTopLeftRadius: 0,
-              borderBottomLeftRadius: 0,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <FontAwesome name="calendar" size={24} color="#ededed" />
-          </View>
-        </TouchableOpacity>
-        <Text style={[styles.fieldLabel, { marginBottom: 20 }]}>
-          {`Travel Duration: ${
-            startDate && endDate
-              ? calculateDaysApart(startDate, endDate) + " days"
-              : ""
-          }`}
-        </Text>
-
+    // <TouchableWithoutFeedback
+    //   style={{ height: "100%" }}
+    //   onPress={Keyboard.dismiss()}
+    // >
+    <View style={styles.container}>
+      <Text style={[global.h3, { marginBottom: 27 }]}>Plan a Trip</Text>
+      <View style={styles.fieldContainer}>
         <TextInput
-          style={{
-            width: "100%",
-            flex: 1,
-            borderRadius: 8,
-            paddingHorizontal: 12,
-            fontFamily: "RobotoLight",
-            backgroundColor: "#ededed",
-            marginBottom: 15,
-            textAlignVertical: "top",
-            backgroundColor: "white",
-            borderWidth: 1,
-            borderColor: "lightgrey",
-            paddingVertical: 12,
-            fontSize: 15,
+          style={styles.fieldInput}
+          placeholder={"Trip name"}
+          placeholderTextColor="black"
+          onChangeText={(name) => {
+            setName(name);
           }}
-          multiline={true}
-          numberOfLines={20}
-          placeholder={"Description"}
-          placeholderTextColor={"black"}
-        />
-
-        <TouchableOpacity style={{ ...styles.submitButton, width: width - 56 }}>
-          <Text
-            style={{ color: "white", fontFamily: "RobotoLight", fontSize: 20 }}
-          >
-            Start Planning
-          </Text>
-        </TouchableOpacity>
-        <DatePicker
-          isVisible={modalOpen}
-          mode={"range"}
-          onConfirm={handleDateConfirm}
-          onCancel={() => setModalOpen(false)}
         />
       </View>
-    </TouchableWithoutFeedback>
+      <TouchableOpacity
+        style={{
+          ...styles.fieldContainer,
+          height: 50,
+          paddingLeft: 12,
+          marginBottom: 0,
+        }}
+        activeOpacity={1}
+        onPress={handleSelectDate}
+      >
+        <Text style={{ width: "37%", fontFamily: "RobotoLight" }}>{`From:\n${
+          startDate ? startDate.toDateString() : ""
+        }`}</Text>
+        <Text style={{ width: "37%", fontFamily: "RobotoLight" }}>{`To:\n${
+          endDate ? endDate.toDateString() : ""
+        }`}</Text>
+        <View
+          style={{
+            backgroundColor: "grey",
+            height: 50,
+            width: 60,
+            borderRadius: 8,
+            borderTopLeftRadius: 0,
+            borderBottomLeftRadius: 0,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <FontAwesome name="calendar" size={24} color="#ededed" />
+        </View>
+      </TouchableOpacity>
+      <Text style={[styles.fieldLabel, { marginBottom: 20 }]}>
+        {`Travel Duration: ${
+          startDate && endDate
+            ? calculateDaysApart(startDate, endDate) + " days"
+            : ""
+        }`}
+      </Text>
+
+      <TextInput
+        style={{
+          width: "100%",
+          flex: 1,
+          borderRadius: 8,
+          paddingHorizontal: 12,
+          fontFamily: "RobotoLight",
+          backgroundColor: "#ededed",
+          marginBottom: 15,
+          textAlignVertical: "top",
+          backgroundColor: "white",
+          borderWidth: 1,
+          borderColor: "lightgrey",
+          paddingVertical: 12,
+          fontSize: 15,
+        }}
+        multiline={true}
+        numberOfLines={20}
+        placeholder={"Description"}
+        placeholderTextColor={"black"}
+        onChangeText={(description) => {
+          setDescription(description);
+        }}
+      />
+
+      <TouchableOpacity
+        style={{ ...styles.submitButton, width: width - 56 }}
+        onPress={handleSubmit}
+      >
+        <Text
+          style={{ color: "white", fontFamily: "RobotoLight", fontSize: 20 }}
+        >
+          Start Planning
+        </Text>
+      </TouchableOpacity>
+      <DatePicker
+        isVisible={modalOpen}
+        mode={"range"}
+        onConfirm={handleDateConfirm}
+        onCancel={() => setModalOpen(false)}
+      />
+    </View>
+    // </TouchableWithoutFeedback>
   );
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,34 +5,62 @@ import {
   Text,
   Button,
   TouchableOpacity,
+  FlatList,
 } from "react-native";
 import { AntDesign, Entypo } from "@expo/vector-icons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodaySection from "../components/TodaySection";
 import global from "../assets/styles/global";
 import CreateTripScreen from "./CreateTripScreen";
+import DatePicker from "react-native-neat-date-picker";
 
 export default function HomeScreen({ navigation }) {
   const [searchSelected, setSearchSelected] = useState(false);
+  const [trips, setTrips] = useState([]);
 
+  const addTrip = (trip) => {
+    setTrips([...trips, trip]);
+  };
+  const customDateString = (date) => {
+    const [month, day, year] = [
+      date.getMonth(),
+      date.getDate(),
+      date.getFullYear(),
+    ];
+    return day + "/" + month + "/" + year;
+  };
   const handleSearchFocus = () => {
     setSearchSelected((state) => !state);
   };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.tripCard}>
+      <Text style={{ fontWeight: "bold" }}>{item.name.toUpperCase()}</Text>
+      <Text>{`${customDateString(item.startDate)} - ${customDateString(
+        item.endDate
+      )}`}</Text>
+    </TouchableOpacity>
+  );
   return (
     <View style={styles.container}>
       <Text style={global.h3}>My Trips</Text>
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.push("CreateTripScreen")}
+        onPress={() =>
+          navigation.push("CreateTripScreen", { addTrip: addTrip })
+        }
       >
         <AntDesign name="plus" size={40} color="white" />
       </TouchableOpacity>
-      <Button
+      {/* <Button
         title={"navigate"}
         onPress={() => {
           navigation.push("ItineraryOverviewScreen");
         }}
-      />
+      /> */}
+      <View style={styles.listWrapper}>
+        <FlatList data={trips} renderItem={renderItem}></FlatList>
+      </View>
     </View>
   );
 }
@@ -56,5 +84,23 @@ const styles = StyleSheet.create({
     backgroundColor: "lightgrey",
     bottom: 30,
     right: 30,
+    zIndex: 2,
+  },
+  tripCard: {
+    width: "100%",
+    backgroundColor: "lightgrey",
+    borderRadius: 8,
+    borderBottomRightRadius: 32,
+    borderTopLeftRadius: 32,
+    height: 100,
+    paddingVertical: 10,
+    paddingHorizontal: 17,
+    justifyContent: "flex-end",
+    marginBottom: 10,
+  },
+  listWrapper: {
+    flex: 1,
+    alignSelf: "stretch",
+    marginTop: 37,
   },
 });
